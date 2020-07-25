79e264ed7b81e7d4ef51f6608b1590f149d6c750
Santhosh Thomas <santhoshthomas015@gmail.com>
2020-07-26 03:37:19 +0530

    seo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,10 +9,10 @@ export class AppComponent implements OnInit {
   title = 'Santhosh Thomas | Front End Web Developer';
 
 
-  imageUrl = 'http://santhoshthomas.xyz/assets/images/profilepic.jpg';
-  site = 'http://santhoshthomas.xyz/';
-  author = 'Santhosh Thomas';
-  description = 'A design-minded front-end software engineer focused on building beautiful interfaces & experiences. I\' m currently pursuing Computer Science and Engineering from APJ Abdul Kalam Technological University. As a software engineer, I enjoy bridging the gap between engineering and design combining my technical knowledge with my keen eye for design to create a beautiful product. My goal is to always build applications that are scalable and efficient under the hood while providing engaging, pixel-perfect user experiences. My goal is to always build applications that are scalable and efficient under the hood while providing engaging, pixel-perfect user experiences.';
+  imageUrl = "http://santhoshthomas.xyz/assets/images/profilepic.jpg";
+  site = "http://santhoshthomas.xyz/";
+  author = "Santhosh Thomas";
+  description = "A design-minded front-end software engineer focused on building beautiful interfaces & experiences. I\' m currently pursuing Computer Science and Engineering from APJ Abdul Kalam Technological University. As a software engineer, I enjoy bridging the gap between engineering and design combining my technical knowledge with my keen eye for design to create a beautiful product. My goal is to always build applications that are scalable and efficient under the hood while providing engaging, pixel-perfect user experiences. My goal is to always build applications that are scalable and efficient under the hood while providing engaging, pixel-perfect user experiences.";
 
  constructor(private metaTagService: Meta,
              private titleService: Title
@@ -30,8 +30,8 @@ export class AppComponent implements OnInit {
     { name: 'creator', content: this.author },
     { name: 'Copyright', content: this.author },
     { name: 'title', content: this.author },
-    // {name: 'robots', content: 'index, follow'},
-    { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+    {name: 'robots', content: 'index, follow'},
+    { name: 'viewport', content: "width=device-width, initial-scale=1" },
     { charset: 'UTF-8' },
 
     // <!-- Twitter -->
