ee9412648cb09a16d12252a0004557678cd290fe
Santhosh Thomas <santhoshthomas015@gmail.com>
2020-07-26 10:45:54 +0530

    seo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,7 @@ export class AppComponent implements OnInit {
 
 
   imageUrl = "http://santhoshthomas.xyz/assets/images/profilepic.jpg";
-  site = "http://santhoshthomas.xyz/";
+  site = "http://santhoshthomas.xyz";
   author = "Santhosh Thomas";
   description = "A design-minded front-end software engineer focused on building beautiful interfaces & experiences. I\' m currently pursuing Computer Science and Engineering from APJ Abdul Kalam Technological University. As a software engineer, I enjoy bridging the gap between engineering and design combining my technical knowledge with my keen eye for design to create a beautiful product. My goal is to always build applications that are scalable and efficient under the hood while providing engaging, pixel-perfect user experiences. My goal is to always build applications that are scalable and efficient under the hood while providing engaging, pixel-perfect user experiences.";
 
@@ -46,7 +46,7 @@ export class AppComponent implements OnInit {
      //  <!-- Open Graph / Facebook -->
     // { name: 'og:type', content: 'website' },
     { name: 'og:url', content: this.site },
-    { property: 'og:type', content: 'website'},
+    { property: 'og:type', content: "website"},
     { name: 'og:title', content: this.author },
     { name: 'og:description', content: this.description },
     { name: 'og:image', content: this.imageUrl },
