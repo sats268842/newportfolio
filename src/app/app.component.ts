bcf91297d14cfed461b6e93fe1d27f5bbe87609a
Santhosh Thomas <santhoshthomas015@gmail.com>
2020-07-26 11:22:38 +0530

    seo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,7 +20,14 @@ export class AppComponent implements OnInit {
 
  ngOnInit(): void {
   this.titleService.setTitle(this.title);
-
+  this.metaTagService.addTags([
+    { name: 'keywords', content: 'Angular SEO Integration, Music CRUD, Angular Universal' },
+    { name: 'robots', content: 'index, follow' },
+    { name: 'author', content: 'Digamber Singh' },
+    { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+    { name: 'date', content: '2019-10-31', scheme: 'YYYY-MM-DD' },
+    { charset: 'UTF-8' }
+  ]);
 }
 
 }
